92946b69c19e0cec40492a2b743f8246591bb7b9
Abrarulctg <jubairctg99@gmail.com>
2024-07-03 20:14:14 +0600

    Added Services Section

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Projects from './Projects';
+import Services from './Services';
 
 const Home = () => {
     return (
         <div className='max-w-6xl mx-auto '>
             <Banner></Banner>
             <Projects></Projects>
-
+            <Services></Services>
         </div>
     );
 };
