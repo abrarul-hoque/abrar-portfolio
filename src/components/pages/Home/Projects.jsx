84532471891550ce8a6a0aeb36c1eeeadc61b0a9
Abrarulctg <jubairctg99@gmail.com>
2024-11-02 22:20:57 +0600

    Added Quizpulse on projects section

--- src/components/pages/Home/Projects.jsx
+++ src/components/pages/Home/Projects.jsx
@@ -13,7 +13,7 @@ import popit from '../../../assets/image/popit.jpg';
 import menufast from '../../../assets/image/menufast.jpg';
 import homesmart from '../../../assets/image/home-smart.jpg';
 import uktell from '../../../assets/image/uktell.png';
-
+import quizpulse from '../../../assets/image/quizpulse.jpg';
 
 
 const Projects = () => {
@@ -21,6 +21,27 @@ const Projects = () => {
         <div className='mb-8 px-4' id="projects">
             <SectionTitle heading={"Projects"} subHeading={"What i have made"} sectionLogo={projectIcon}></SectionTitle>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {/* Single Project */}
+                <div className="w-full border-4 border-white hover:border-red-500 rounded-xl single-project">
+                    <div className="hero rounded-xl h-full" style={{ backgroundImage: `url(${quizpulse})`, }}>
+                        <div className="hero-overlay  rounded-xl bg-opacity-70"></div>
+                        <div className="hero-content pt-28 text-neutral-content text-center">
+                            <div className="max-w-md">
+                                <h1 className="mb-4 text-3xl font-bold">QuizPulse</h1>
+                                <p className="mb-3 text-base">
+                                    An Interactive Quiz Platform
+                                </p>
+                                <div className='project-action '>
+                                    <a href="https://github.com/mdmarufhossianbd/quizpluse" target='_blank'><button className='btn btn-sm text-[#002366] mr-2'><FaGithub /> Code</button></a>
+                                    <a href="https://quizpluse.vercel.app/" target='_blank'><button className='btn btn-sm text-[#002366]'><PiCursorClickFill />
+                                        Live</button></a>
+                                    {/* <button className="btn btn-primary">Live</button> */}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {/* Single Project */}
                 <div className="w-full border-4 border-white hover:border-red-500 rounded-xl single-project">
                     <div className="hero rounded-xl" style={{ backgroundImage: `url(${fhdc})`, }}>
