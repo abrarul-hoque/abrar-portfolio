23557817702e9d4a8ad9124df3229f4b1a9533d2
Abrarulctg <jubairctg99@gmail.com>
2024-12-18 18:25:34 +0600

    Added new resume link

--- src/components/pages/Home/Banner.jsx
+++ src/components/pages/Home/Banner.jsx
@@ -8,19 +8,19 @@ import { Link } from 'react-router-dom';
 const Banner = () => {
     return (
         <div className='flex flex-col lg:flex-row gap-2 my-2 items-center'>
-            <div className='w-full lg:w-5/12 text-center p-2'>
+            <div className='w-full lg:w-6/12 text-center p-2'>
                 <h4 className='text-2xl'>Welcome, I'm Abrarul Hoque </h4>
-                <h1 className="text-5xl my-3 font-extrabold colorText">Web Developer</h1>
+                <h1 className="text-5xl my-3 font-extrabold colorText">Frontend Developer</h1>
                 {/* <h6 className="text-base">Expert JavaScript Developer & Innovator in Modern Web Technologies</h6> */}
-                <h6 className="text-base">Accomplished JavaScript Developer and Innovator in Modern Web Technologies, renowned for creating sophisticated applications with deep expertise in JavaScript, React, and Firebase.</h6>
+                <h6 className="text-base">Accomplished JavaScript Developer and Innovator in Modern Web Technologies, renowned for creating sophisticated applications with deep expertise in JavaScript, React and Firebase.</h6>
                 <div className='my-8'>
-                    <a href="https://drive.google.com/uc?export=download&id=1kesPHxFNLNvwI4dkmpY8u2eTsNoBSW3U">
+                    <a href="https://drive.google.com/uc?export=download&id=1Ez0jZMyMZ6JkUAezXOFtg0owMuOpjUaF">
                         <button className='btn mr-2'><FaDownload /> Download CV</button>
                     </a>
                     <Link to="/contact" className='btn bg-[#002366] hover:bg-white text-white hover:text-[#002366] border-2'>Contact</Link>
                 </div>
             </div>
-            <div className='w-full lg:w-6/12 '>
+            <div className='w-full lg:w-5/12 '>
                 <img src={bannerImg} alt="" />
             </div>
             <div className='w-1/12 hidden lg:block social-link'>
